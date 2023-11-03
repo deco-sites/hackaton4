27107539ac5b0b68c876906ed50f373dca89b7d0
Guilhermecheng <guilherme.cheng@gmail.com>
2023-11-03 13:51:59 -0300

    hero

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,5 +1,6 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
+import CampaignTimer, { Props as CampaignTimerProps } from "../header/CampaignTimer.tsx";
 
 export interface Props {
   backgroundImage: {
@@ -7,42 +8,33 @@ export interface Props {
     mobile: ImageWidget;
     alt: string;
     lcp?: boolean;
+    campaignTimer?: CampaignTimerProps;
   };
 }
 
-export default function Hero({ backgroundImage }: Props) {
+export default function Hero({ backgroundImage, campaignTimer }: any) {
   if (!backgroundImage) return null;
 
-  return (
-    <div class="w-full h-[95vh]">
-      <a
-        href={"#"}
-        aria-label="Banner"
-        class="relative h-[600px] overflow-y-hidden w-full"
-      >
-        <Picture preload={backgroundImage.lcp}>
-          <Source
-            media="(max-width: 767px)"
-            fetchPriority={backgroundImage.lcp ? "high" : "auto"}
-            src={backgroundImage.mobile}
-            width={360}
-            height={600}
-          />
-          <Source
-            media="(min-width: 768px)"
-            fetchPriority={backgroundImage.lcp ? "high" : "auto"}
-            src={backgroundImage.desktop}
-            width={1440}
-            height={600}
-          />
-          <img
-            class="object-cover w-full h-full"
-            loading={backgroundImage.lcp ? "eager" : "lazy"}
-            src={backgroundImage.desktop}
-            alt={backgroundImage.alt}
-          />
-        </Picture>
-      </a>
-    </div>
+  return ( 
+    <>
+      <div class="z-0 fixed top-0 left-0 h-[80vh] w-[50vw]" style={`background-image: url('${backgroundImage.desktop}'); background-size:cover; background-repeat: no-repeat; background-position: center;`} />
+      <div class="hero w-full h-[80vh] flex flex-col items-center justify-center md:grid md:grid-cols-5">
+        <div class="flex flex-col w-full pt-20 h-[20%] md:h-full items-center md:items-end bg-opacity-60 md:col-span-2">
+          <div class="relative w-full after:absolute after:left-10 after:top-0 after:content-[''] after:bg-red-600/80 after:h-[8px] after:w-[80%]"></div>
+          <div class="relative w-full after:absolute after:left-10 after:top-10 after:content-[''] after:bg-red-600/80 after:h-[6px] after:w-[60%]"></div>
+        </div>
+        
+        <div class="hero-content text-neutral-content w-full md:h-full md:col-start-3 md:col-span-3 md:bg-black">
+          <div class="w-[80%] md:w-[95%] bg-black p-8">
+            <h1 class="mb-4 lg:mb-8 text-5xl lg:text-7xl font-bold">BLACK FRIDAY</h1>
+            <p class="mb-4 lg:mb-8 lg:text-2xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary bg-red-600/80">Quero meu desconto agora!</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <CampaignTimer {...campaignTimer} />
+      </div>
+    </>
   );
 }
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -3,38 +3,38 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
-import * as $$$$$$0 from "./sections/Category/CategoryBanner.tsx";
-import * as $$$$$$1 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$2 from "./sections/Content/Benefits.tsx";
-import * as $$$$$$3 from "./sections/Content/BrandCarousel.tsx";
-import * as $$$$$$4 from "./sections/Content/Cover.tsx";
-import * as $$$$$$5 from "./sections/Content/Faq.tsx";
-import * as $$$$$$6 from "./sections/Content/Hero.tsx";
-import * as $$$$$$7 from "./sections/Content/Logos.tsx";
-import * as $$$$$$8 from "./sections/Content/SectionBridge.tsx";
-import * as $$$$$$9 from "./sections/Content/StatsTestimonials.tsx";
-import * as $$$$$$10 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$11 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$12 from "./sections/Footer/FooterLP.tsx";
-import * as $$$$$$13 from "./sections/Header/Header.tsx";
-import * as $$$$$$14 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$15 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$16 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$17 from "./sections/Images/ImageParallax.tsx";
-import * as $$$$$$18 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$19 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$20 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$21 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$22 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$23 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$24 from "./sections/Newsletter/NewsletterModal.tsx";
-import * as $$$$$$25 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$26 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$27 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$28 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$29 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$30 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$31 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$0 from "./sections/Footer/FooterLP.tsx";
+import * as $$$$$$1 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$2 from "./sections/Category/CategoryBanner.tsx";
+import * as $$$$$$3 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$4 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$5 from "./sections/Images/ImageParallax.tsx";
+import * as $$$$$$6 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$7 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$8 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$9 from "./sections/Content/Hero.tsx";
+import * as $$$$$$10 from "./sections/Content/SectionBridge.tsx";
+import * as $$$$$$11 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$12 from "./sections/Content/Cover.tsx";
+import * as $$$$$$13 from "./sections/Content/BrandCarousel.tsx";
+import * as $$$$$$14 from "./sections/Content/Logos.tsx";
+import * as $$$$$$15 from "./sections/Content/Faq.tsx";
+import * as $$$$$$16 from "./sections/Content/StatsTestimonials.tsx";
+import * as $$$$$$17 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$18 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$19 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$20 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$21 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$22 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$23 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$24 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$25 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$26 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$27 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$28 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$29 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$30 from "./sections/Newsletter/NewsletterModal.tsx";
+import * as $$$$$$31 from "./sections/Header/Header.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -43,41 +43,38 @@ const manifest = {
     "deco-sites/hackaton4/loaders/Layouts/ProductCard.tsx": $$$0,
   },
   "sections": {
-    "deco-sites/hackaton4/sections/Category/CategoryBanner.tsx": $$$$$$0,
-    "deco-sites/hackaton4/sections/Category/CategoryList.tsx": $$$$$$1,
-    "deco-sites/hackaton4/sections/Content/Benefits.tsx": $$$$$$2,
-    "deco-sites/hackaton4/sections/Content/BrandCarousel.tsx": $$$$$$3,
-    "deco-sites/hackaton4/sections/Content/Cover.tsx": $$$$$$4,
-    "deco-sites/hackaton4/sections/Content/Faq.tsx": $$$$$$5,
-    "deco-sites/hackaton4/sections/Content/Hero.tsx": $$$$$$6,
-    "deco-sites/hackaton4/sections/Content/Logos.tsx": $$$$$$7,
-    "deco-sites/hackaton4/sections/Content/SectionBridge.tsx": $$$$$$8,
-    "deco-sites/hackaton4/sections/Content/StatsTestimonials.tsx": $$$$$$9,
-    "deco-sites/hackaton4/sections/Content/Testimonials.tsx": $$$$$$10,
-    "deco-sites/hackaton4/sections/Footer/Footer.tsx": $$$$$$11,
-    "deco-sites/hackaton4/sections/Footer/FooterLP.tsx": $$$$$$12,
-    "deco-sites/hackaton4/sections/Header/Header.tsx": $$$$$$13,
-    "deco-sites/hackaton4/sections/Images/BannerGrid.tsx": $$$$$$14,
-    "deco-sites/hackaton4/sections/Images/Carousel.tsx": $$$$$$15,
-    "deco-sites/hackaton4/sections/Images/ImageGallery.tsx": $$$$$$16,
-    "deco-sites/hackaton4/sections/Images/ImageParallax.tsx": $$$$$$17,
-    "deco-sites/hackaton4/sections/Images/ShoppableBanner.tsx": $$$$$$18,
-    "deco-sites/hackaton4/sections/Links/LinkTree.tsx": $$$$$$19,
-    "deco-sites/hackaton4/sections/Links/Shortcuts.tsx": $$$$$$20,
-    "deco-sites/hackaton4/sections/Miscellaneous/CampaignTimer.tsx":
-      $$$$$$21,
-    "deco-sites/hackaton4/sections/Miscellaneous/CookieConsent.tsx":
-      $$$$$$22,
-    "deco-sites/hackaton4/sections/Newsletter/Newsletter.tsx": $$$$$$23,
-    "deco-sites/hackaton4/sections/Newsletter/NewsletterModal.tsx":
-      $$$$$$24,
-    "deco-sites/hackaton4/sections/Product/ProductDetails.tsx": $$$$$$25,
-    "deco-sites/hackaton4/sections/Product/ProductShelf.tsx": $$$$$$26,
-    "deco-sites/hackaton4/sections/Product/SearchResult.tsx": $$$$$$27,
-    "deco-sites/hackaton4/sections/Product/Wishlist.tsx": $$$$$$28,
-    "deco-sites/hackaton4/sections/Social/InstagramPosts.tsx": $$$$$$29,
-    "deco-sites/hackaton4/sections/Social/WhatsApp.tsx": $$$$$$30,
-    "deco-sites/hackaton4/sections/Theme/Theme.tsx": $$$$$$31,
+    "deco-sites/hackaton4/sections/Category/CategoryBanner.tsx": $$$$$$2,
+    "deco-sites/hackaton4/sections/Category/CategoryList.tsx": $$$$$$3,
+    "deco-sites/hackaton4/sections/Content/Benefits.tsx": $$$$$$17,
+    "deco-sites/hackaton4/sections/Content/BrandCarousel.tsx": $$$$$$13,
+    "deco-sites/hackaton4/sections/Content/Cover.tsx": $$$$$$12,
+    "deco-sites/hackaton4/sections/Content/Faq.tsx": $$$$$$15,
+    "deco-sites/hackaton4/sections/Content/Hero.tsx": $$$$$$9,
+    "deco-sites/hackaton4/sections/Content/Logos.tsx": $$$$$$14,
+    "deco-sites/hackaton4/sections/Content/SectionBridge.tsx": $$$$$$10,
+    "deco-sites/hackaton4/sections/Content/StatsTestimonials.tsx": $$$$$$16,
+    "deco-sites/hackaton4/sections/Content/Testimonials.tsx": $$$$$$11,
+    "deco-sites/hackaton4/sections/Footer/Footer.tsx": $$$$$$1,
+    "deco-sites/hackaton4/sections/Footer/FooterLP.tsx": $$$$$$0,
+    "deco-sites/hackaton4/sections/Header/Header.tsx": $$$$$$31,
+    "deco-sites/hackaton4/sections/Images/BannerGrid.tsx": $$$$$$6,
+    "deco-sites/hackaton4/sections/Images/Carousel.tsx": $$$$$$8,
+    "deco-sites/hackaton4/sections/Images/ImageGallery.tsx": $$$$$$7,
+    "deco-sites/hackaton4/sections/Images/ImageParallax.tsx": $$$$$$5,
+    "deco-sites/hackaton4/sections/Images/ShoppableBanner.tsx": $$$$$$4,
+    "deco-sites/hackaton4/sections/Links/LinkTree.tsx": $$$$$$27,
+    "deco-sites/hackaton4/sections/Links/Shortcuts.tsx": $$$$$$28,
+    "deco-sites/hackaton4/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$22,
+    "deco-sites/hackaton4/sections/Miscellaneous/CookieConsent.tsx": $$$$$$23,
+    "deco-sites/hackaton4/sections/Newsletter/Newsletter.tsx": $$$$$$29,
+    "deco-sites/hackaton4/sections/Newsletter/NewsletterModal.tsx": $$$$$$30,
+    "deco-sites/hackaton4/sections/Product/ProductDetails.tsx": $$$$$$21,
+    "deco-sites/hackaton4/sections/Product/ProductShelf.tsx": $$$$$$20,
+    "deco-sites/hackaton4/sections/Product/SearchResult.tsx": $$$$$$19,
+    "deco-sites/hackaton4/sections/Product/Wishlist.tsx": $$$$$$18,
+    "deco-sites/hackaton4/sections/Social/InstagramPosts.tsx": $$$$$$25,
+    "deco-sites/hackaton4/sections/Social/WhatsApp.tsx": $$$$$$24,
+    "deco-sites/hackaton4/sections/Theme/Theme.tsx": $$$$$$26,
   },
   "apps": {
     "deco-sites/hackaton4/apps/decohub.ts": $$$$$$$$$$$0,
