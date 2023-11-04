1a53fd815c6ff0127e8d867be39192de118fa2d5
Guilhermecheng <guilherme.cheng@gmail.com>
2023-11-04 01:39:05 -0300

    mudancas

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero({ backgroundImage, campaignTimer }: any) {
 
   return ( 
     <>
-      <div class="z-0 fixed top-0 left-0 h-[80vh] w-[50vw]" style={`background-image: url('${backgroundImage.desktop}'); background-size:cover; background-repeat: no-repeat; background-position: center;`} />
+      <div class="z-[-1] fixed top-0 left-0 h-[80vh] w-[50vw]" style={`background-image: url('${backgroundImage.desktop}'); background-size:cover; background-repeat: no-repeat; background-position: center;`} />
       <div class="hero w-full h-[80vh] flex flex-col items-center justify-center md:grid md:grid-cols-5">
         <div class="flex flex-col w-full pt-20 h-[20%] md:h-full items-center md:items-end bg-opacity-60 md:col-span-2">
           <div class="relative w-full after:absolute after:left-10 after:top-0 after:content-[''] after:bg-red-600/80 after:h-[8px] after:w-[80%]"></div>
@@ -25,14 +25,14 @@ export default function Hero({ backgroundImage, campaignTimer }: any) {
         </div>
         
         <div class="hero-content text-neutral-content w-full md:h-full md:col-start-3 md:col-span-3 md:bg-black">
-          <div class="w-[80%] md:w-[95%] bg-black p-8">
+          <div class="w-[90%] md:w-[95%] bg-black px-8 py-12">
             <h1 class="mb-4 lg:mb-8 text-5xl lg:text-7xl font-bold">BLACK FRIDAY</h1>
             <p class="mb-4 lg:mb-8 lg:text-2xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
             <button class="btn btn-primary bg-red-600/80">Quero meu desconto agora!</button>
           </div>
         </div>
       </div>
-      <div>
+      <div class="z-10 w-full flex items-center justify-center bg-red-700 h-40 text-lg font-semibold">
         <CampaignTimer {...campaignTimer} />
       </div>
     </>
